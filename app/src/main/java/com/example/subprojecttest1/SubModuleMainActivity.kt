c1fb90d5ecdb94dd697f91c702efa82e3dbaec8d
liurui <303996789@qq.com>
2023-07-25 13:58:43 +0800

    添加页面

---
+++ app/src/main/java/com/example/subprojecttest1/SubModuleMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.subprojecttest1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubModuleMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_module_main)
+    }
+}
